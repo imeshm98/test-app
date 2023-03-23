4bbad383448a5da4e75b3016f160588b76a80fde
imeshm98 <imeshmofficial@gmail.com>
2023-03-23 09:47:34 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:17
+EXPOSE 8080
+ADD target/test-app.jar test-app.jar
+ENTRYPOINT ["java" , "-jar" , "/test-app.jar"]
---
+++ src/main/java/testapp/testapp/TestAppApplication.java
@@ -0,0 +1,22 @@
+package testapp.testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppApplication {
+
+	public static void main(String[] args) {
+
+		int x =1;
+
+		if(x==0){
+			System.out.println("Result is 0");
+		} else if(x==1){
+			System.out.println("Result is 1");
+		} else if(x==2){
+			System.out.println("Result is 2");
+		}
+	}
+
+}
